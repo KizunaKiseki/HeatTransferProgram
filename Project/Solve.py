6084fea727d0800717c7a4fe341a0552749ef280
KizunaKiseki <74109635+KizunaKiseki@users.noreply.github.com>
2026-03-24 12:04:48 -0500

    Updated Files to contain only necessary information

--- Project/Solve.py
+++ Project/Solve.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-TITLE = [INSERT TITLE HERE]
-DATE  = 2025.01.01
+TITLE = Solving Functions
+DATE  = 2026.05.15
 _____________________________________________________________________
 DESCRIPTION:
 1. [Insert Description Here]
 2. ...
-_____________________________________________________________________
-HOW-TO:
--> Click on the button
--> ...
-_____________________________________________________________________
-UPDATES:
-- [2025.02.02] - 1.1 ...
-- [2025.01.01] - 1.0 RELEASE
 _____________________________________________________________________
 TO-DO:
 - Describe Next Features
@@ -52,55 +44,6 @@ def function_name():
     
     pass
 
-
-# * CLASSES *
-# ? ================================================================ ?
-
-class ClassName:
-    """
-    Summary of what the class does
-    
-    Args:
-    
-    
-    Returns:
-    
-    
-    Raises:
-    """
-    
-    def __init__(self):
-        """
-        Initializes a new instance of the ClassName class.
-        
-        Args:
-            - self (ClassName): The instance of the class.
-        
-        Returns:
-            
-        
-        Raises:
-        """
-        
-        
-        pass
-
-    def method_name(self):
-        """
-        Summary of what the method does
-        
-        Args:
-            - self (ClassName): The instance of the class.
-        
-        Returns:
-        
-        
-        Raises:
-        """
-        
-        
-        
-        pass
 
 
 # * MAIN *
