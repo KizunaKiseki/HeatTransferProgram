8d2e8511477b44bdbae2db3871880f35153211c3
KizunaKiseki <74109635+KizunaKiseki@users.noreply.github.com>
2026-05-07 09:13:14 -0500

    Created Project Submission folder to update code so that it is ready to be submitted.

---
+++ ProjectSubmission/lib/_Store.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+TITLE = Store Functions
+DATE  = 2026.05.15
+_____________________________________________________________________
+DESCRIPTION:
+1. Functions for reading, writing, and printing mesh data.
+2. A function for extracting node coordinates from the mesh data.
+_____________________________________________________________________
+DISCLAIMER:
+- Portions of this code were developed with AI autocomplete tools.
+- These tools were used to improve coding efficiency and syntax accuracy.
+_____________________________________________________________________
+AUTHOR : Nicholas Heling
+"""
+
+# * IMPORTS *
+# ? ================================================================ ?
+
+# ! PYTHON TEMPLATES & LIBRARIES !
+import pandas as pd
+
+# * VARIABLES *
+# ? ================================================================ ?
+N1 = ['n0','n1']
+N2 = ['n0','n1','n2']
+
+# * FUNCTION *
+# ? ================================================================ ?
+
+def read_mesh(file_name : str) -> dict:
+    """
+    This function reads a mesh from an Excel file and returns it as a dictionary.
+    
+    Args:
+        file_name (str): The name of the Excel file containing the mesh data.
+    
+    Returns:
+        mesh (dict): A dictionary containing the mesh data.
+    
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        Exception: For any I/O errors or issues while reading the file.
+    """
+    # Types for all columns in short form
+    types = {
+        str: 'name color',
+        int: 'id material n0 n1 n2',
+        float: 'x y T q rho k C',
+    }
+    
+    # Invert types dictionary for pandas format
+    conv = {
+        column: dtype
+        for dtype, columns in types.items()
+        for column in columns.split()
+    }
+
+    # Read all sheets in file as dataframes
+    mesh = pd.read_excel(file_name, None, converters=conv)
+    
+    
+    return mesh
+
+
+def write_mesh(mesh : dict, file_name : str) -> None:
+    """
+    This function writes a mesh dictionary to an Excel file.
+    
+    Args:
+        mesh (dict): A dictionary containing the mesh data.
+        file_name (str): The name of the Excel file.
+    
+    Returns:
+        None
+    """
+    with pd.ExcelWriter(file_name) as excel:
+        for (sheet, data) in mesh.items():
+            data.to_excel(excel, sheet_name=sheet, index=False)
+
+
+def print_mesh(mesh : dict) -> None:
+    """
+    This function prints the contents of a mesh dictionary in a readable format.
+    
+    Args:
+        mesh (dict): A dictionary containing the mesh data.
+    
+    Returns:
+        None
+    """
+    for (k, (sheet, data)) in enumerate(mesh.items()):
+        if k != 0:
+            print()
+        
+        print(f'= {sheet} ='.center(46, '='))
+        print(data)
+
+
+def get_nodes(mesh : dict) -> tuple:
+    """
+    This function gets the x and y coordinates of the nodes from the mesh dictionary.
+    
+    Args:
+        mesh (dict): A dictionary containing the mesh data.
+    
+    Returns:
+        x_coordinate (tuple): A tuple containing the x coordinates of the nodes.
+        y_coordinate (tuple): A tuple containing the y coordinates of the nodes.
+    """
+    x_coordinate = mesh['XY']['x'].values
+    y_coordinate = mesh['XY']['y'].values
+    
+    
+    return x_coordinate, y_coordinate
+
---
+++ ProjectSubmission/snippets/solver.py
@@ -0,0 +1,128 @@
+import numpy as np
+import scipy.sparse as sp
+
+from snippets.storage import ℕ1,ℕ2
+from snippets.storage import get_nodes
+
+import matplotlib.pyplot as pl
+import snippets.plotting as plotting
+
+
+Δz = 0.01 # m
+
+
+def build_x(x,y):
+	x = np.array([x,y]).T
+	return x
+
+
+def build_X(x,y):
+	X = np.array([[1,1,1],x-x.mean(),y-y.mean()]).T
+	return X
+
+
+def build_capacity(mesh):
+	Nₙ = mesh['XY'].shape[0]
+	Nₑ = mesh['IE'].shape[0]
+	C = sp.dok_array((Nₙ,Nₙ))
+	
+	xₙ,yₙ = get_nodes(mesh)
+	nodes = mesh['IE'][ℕ2].values
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		xₜ = xₙ[n]
+		yₜ = yₙ[n]
+		
+		# TODO: Implement Equations 22
+		
+		C[I,J] += Cₑ
+	
+	return C
+
+
+def build_conduction(mesh):
+	Nₙ = mesh['XY'].shape[0]
+	Nₑ = mesh['IE'].shape[0]
+	K = sp.dok_array((Nₙ,Nₙ))
+	
+	xₙ,yₙ = get_nodes(mesh)
+	nodes = mesh['IE'][ℕ2].values
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		xₜ = xₙ[n]
+		yₜ = yₙ[n]
+		
+		# TODO: Implement Equation 18
+  
+		pass
+		
+		I = n.reshape(3,1)
+		J = n.reshape(1,3)
+		K[I,J] += Kₑ
+	
+	return K
+
+
+def build_generation(mesh):
+	Nₙ = mesh['XY'].shape[0]
+	Nₑ = mesh['IE'].shape[0]
+	E = np.zeros(Nₙ)
+	
+	xₙ,yₙ = get_nodes(mesh)
+	nodes = mesh['IE'][ℕ2].values
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		xₜ = xₙ[n]
+		yₜ = yₙ[n]
+		
+		# TODO: Equation 39 ?
+		
+  
+		pass
+		
+		E[n] += e*V
+	
+	return E
+
+
+def steady_BCs(mesh,K,E):
+	Nₑ = mesh['BE'].shape[0]
+	xₙ,yₙ = get_nodes(mesh)
+	nodes = mesh['BE'][ℕ1].values
+	
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		cid = mesh['BE']['cid'][kₑ]
+		qₑ = mesh['BC']['q'][cid]
+		
+		if np.isfinite(qₑ):
+      
+			# TODO: Implement Equation 39 
+			pass
+	
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		cid = mesh['BE']['cid'][kₑ]
+		Tₑ = mesh['BC']['T'][cid]
+		if np.isfinite(Tₑ):
+      
+			# TODO: Fixed Temperature
+			pass
+
+
+def transient_BCs(mesh,C,K,E,T):
+	Nₑ = mesh['BE'].shape[0]
+	xₙ,yₙ = get_nodes(mesh)
+	nodes = mesh['BE'][ℕ1].values
+	for kₑ in range(Nₑ):
+		n = nodes[kₑ]
+		
+		cid = mesh['BE']['cid'][kₑ]
+		Tₑ = mesh['BC']['T'][cid]
+		qₑ = mesh['BC']['q'][cid]
+		if np.isfinite(Tₑ):
+			# TODO: 
+			pass
+		if np.isfinite(qₑ):
+			# TODO:
+			pass
