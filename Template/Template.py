0342a8f804f78e9b9b110e292690adc76de383c0
KizunaKiseki <74109635+KizunaKiseki@users.noreply.github.com>
2026-09-03 16:31:50 -0500

    Added Template code back to project file.

---
+++ Template/Template.py
@@ -0,0 +1,134 @@
+# --- coding: utf-8 -*-
+"""
+TITLE = [INSERT TITLE HERE]
+DATE  = 2026.01.01
+_____________________________________________________________________
+DESCRIPTION:
+Insert Description Here
+_____________________________________________________________________
+HOW-TO:
+1. Step 1...
+2. ...
+_____________________________________________________________________
+TO-DO:
+[FEATURE] - Describe Feature
+[BUG] - Describe Bug
+...
+_____________________________________________________________________
+UPDATES:
+- [2026.02.02] 0.0.1 ...
+- [2026.01.01] 0.0.0 RELEASE
+_____________________________________________________________________
+AUTHOR: [Author]
+"""
+
+# * IMPORTS *
+# ? ================================================================ ?
+
+# ! PYTHON TEMPLATES & LIBRARIES !
+
+
+# ! PROJECT MODULES !
+
+
+# * VARIABLES *
+# ? ================================================================ ?
+
+
+# * FUNCTION *
+# ? ================================================================ ?
+
+def function_name():
+    """
+    Summary of what the function does
+    
+    Args:
+    
+    
+    Returns:
+    
+    
+    Raises:
+    """
+    
+    
+    
+    pass
+
+
+# * CLASSES *
+# ? ================================================================ ?
+
+class ClassName:
+    """
+    Summary of what the class does
+    
+    Args:
+    
+    
+    Returns:
+    
+    
+    Raises:
+    """
+    
+    def __init__(self):
+        """
+        Initializes a new instance of the ClassName class.
+        
+        Args:
+            - self (ClassName): The instance of the class.
+        
+        Returns:
+            
+        
+        Raises:
+        """
+        
+        
+        pass
+
+    def method_name(self):
+        """
+        Summary of what the method does
+        
+        Args:
+            - self (ClassName): The instance of the class.
+        
+        Returns:
+        
+        
+        Raises:
+        """
+        
+        
+        
+        pass
+
+
+# * MAIN *
+# ? ================================================================ ?
+
+def main():
+    """
+    Summary of what the main does
+    
+    Args:
+    
+    
+    Returns:
+    
+    
+    Raises:
+    """
+    # Code Here
+    
+    
+    pass
+
+        
+        
+# * EXECUTE *
+# ? ================================================================ ?
+if __name__ == "__main__":
+    main()
